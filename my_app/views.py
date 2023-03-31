a6a9955664be3f09fdcbf44c0b2c98516020d403
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-03-31 12:26:27 -0700

    frontend search results almost work with test

--- my_app/views.py
+++ my_app/views.py
@@ -5,4 +5,9 @@ def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
-    return render(request, 'my_app/new_search.html')
+    #request.POST returns a dictionary, .get accesses the value at 'search'
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
