fe00de83db8892c32a8af94111f3c59f7d408112
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-04 16:41:51 -0700

    for loop and parsing

--- my_app/views.py
+++ my_app/views.py
@@ -20,8 +20,30 @@ def new_search(request):
     response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
-    post_titles = soup.find_all('li', {'class': 'cl-search-result cl-search-view-mode-list'})
-    print(data)
+    post_listings = soup.find_all('li', {'class': 'cl-search-result cl-search-view-mode-list'})
+
+    final_postings = []
+    
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(
+                class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append(
+            (post_title, post_url, post_price, post_image_url))
+    
     
     stuff_for_frontend = {
         'search': search,
