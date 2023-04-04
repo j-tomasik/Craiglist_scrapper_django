a9fda4b5345088b68f23304dd91209c3c17ee828
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-04-04 11:32:08 -0700

    search parser

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
+
+BASE_CRAIGSLIST_URL = 'http://sanfrancisco.craigslist.org/search/?query={}'
+
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
@@ -7,7 +12,11 @@ def home(request):
 def new_search(request):
     #request.POST returns a dictionary, .get accesses the value at 'search'
     search = request.POST.get('search')
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+
+    response = requests.get('https://sfbay.craigslist.org/search/sss?query=computer#search=1~gallery~0~0')
+    data = response.text
     stuff_for_frontend = {
         'search': search,
     }
-    return render(request, 'my_app/new_search.html', stuff_for_frontend)
+    return render(request, 'my_app/new_search.html', stuff_for_frontend) 
